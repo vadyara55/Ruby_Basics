ca3f6f36533cee66fe0699a63da12dd9c841625d
vadyara55 <46928549+vadyara55@users.noreply.github.com>
2019-02-05 16:26:53 +0200

    Add files via upload

--- lesson1/equation.rb
+++ lesson1/equation.rb
@@ -1,1 +1,17 @@
-
+puts "Дано уравнение : ax^2+bx+c=0"
+puts "Введите коэфициенты a ,b ,c "
+a = gets.to_f
+b = gets.to_f
+c = gets.to_f
+d = b**2 - 4.0 * a * c
+if d < 0
+  puts "Действительных корней нет"
+elsif d > 0
+  sqrt = Math.sqrt(d)
+  x1 = -b + sqrt / (2.0 * a)
+  x2 = -b - sqrt / (2.0 * a)
+  puts "Первый корень = #{x1}, второй = #{x2}"
+else
+x3 = -b / (2.0 * a)
+puts "Корень уравнения = #{x3}"
+end
---
+++ lesson1/square.rb
@@ -0,0 +1,6 @@
+puts "Введите высоту треугольника"
+height = gets.to_f
+puts "введите основание треугольника"
+base = gets.to_f
+area = 0.5 * height * base
+puts area
